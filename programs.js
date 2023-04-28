613df85772e760a18713ec9d59a238118700983f
dhanigill <132022244+dhanigill@users.noreply.github.com>
2023-04-28 11:28:25 -0500

    Add files via upload

--- programs.js
+++ programs.js
@@ -1,8 +1,8 @@
 document.addEventListener("DOMContentLoaded", function () {
-  
   function fadeIn(elem) {
     if (!elem) return;
-    let distInView = elem.getBoundingClientRect().top - window.innerHeight + 100;
+    let distInView =
+      elem.getBoundingClientRect().top - window.innerHeight + 100;
     if (distInView < 0) {
       elem.classList.add("inView");
     } else {
@@ -24,30 +24,45 @@ document.addEventListener("DOMContentLoaded", function () {
 
   //calling function for different divs
   let elemName = document.querySelector(".mname");
-  window.addEventListener("scroll", () =>fadeIn(elemName));
-  window.addEventListener("scroll", () =>fadeOut(elemName));
+  window.addEventListener("scroll", () => fadeIn(elemName));
+  window.addEventListener("scroll", () => fadeOut(elemName));
 
   let elemBio = document.querySelector(".bio");
-  window.addEventListener("scroll", () =>fadeIn(elemBio));
-  window.addEventListener("scroll", () =>fadeOut(elemBio));
-  
-  let elemEducation=document.querySelector(".Education");
-  window.addEventListener("scroll",() => fadeIn(elemEducation));
-  window.addEventListener("scroll",() => fadeOut(elemEducation));
-  
-  let elemWork=document.querySelector(".Work");
-  window.addEventListener("scroll",() => fadeIn(elemWork));
-  window.addEventListener("scroll",() => fadeOut(elemWork));
+  window.addEventListener("scroll", () => fadeIn(elemBio));
+  window.addEventListener("scroll", () => fadeOut(elemBio));
 
+  let elemEducation = document.querySelector(".Education");
+  window.addEventListener("scroll", () => fadeIn(elemEducation));
+  window.addEventListener("scroll", () => fadeOut(elemEducation));
+
+  let elemWork = document.querySelector(".Work");
+  window.addEventListener("scroll", () => fadeIn(elemWork));
+  window.addEventListener("scroll", () => fadeOut(elemWork));
 
   //function for Nav bar active link color
 
-  const tab=document.querySelectorAll(".header_link");
-  const windowHref=document.location.pathname;
-  tab.forEach(tablink =>{
-    if(tablink.href.includes(windowHref)){
-      tablink.classList.add('active');
+  const tab = document.querySelectorAll(".header_link");
+  const windowHref = document.location.pathname;
+  tab.forEach((tablink) => {
+    if (tablink.href.includes(windowHref)) {
+      tablink.classList.add("active");
     }
-  })
-  
+  });
+
+  const Pname = document.getElementsByClassName("pname");
+  const header = document.getElementsByClassName("header");
+
+  const handler = new IntersectionObserver((entries) => {
+    if (header) {
+      if (!entries[0].isIntersecting) {
+        console.log("show");
+        header[0].classList.add("shadow");
+      } else {
+        console.log("remove");
+        header[0].classList.remove("shadow");
+      }
+    }
+  });
+
+  handler.observe(Pname[0]);
 });
