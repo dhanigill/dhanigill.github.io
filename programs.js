95a565bef5de76cf1fec1b63aa82e84dc0a85045
dhanigill <132022244+dhanigill@users.noreply.github.com>
2023-05-05 21:13:16 -0500

    Add files via upload

--- programs.js
+++ programs.js
@@ -1,4 +1,13 @@
 document.addEventListener("DOMContentLoaded", function () {
+  const mainnav = document.querySelector(".nav");
+  const hambutton = document.querySelector("#menu");
+
+  // Add a click event listender to the hamburger button and use a callback function that toggles the list element's list of classes.
+  hambutton.addEventListener("click", () => {
+    mainnav.classList.toggle("show");
+    hambutton.classList.toggle("show");
+  });
+
   //function for Nav bar active link color
 
   const tab = document.querySelectorAll(".header_link");
