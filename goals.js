5d6cd0e328f43aaf73a0e3eeebea7bfca42c0873
dhanigill <132022244+dhanigill@users.noreply.github.com>
2023-05-06 19:03:03 -0500

    Add files via upload

--- goals.js
+++ goals.js
@@ -2,7 +2,6 @@ document.addEventListener("DOMContentLoaded", function () {
   // Select all slides
   const slides = document.querySelectorAll(".sliderItem");
 
- 
   slides.forEach((sliderItem, indx) => {
     sliderItem.style.transform = `translateX(${indx * 100}%)`;
   });
@@ -12,12 +11,20 @@ document.addEventListener("DOMContentLoaded", function () {
   let curSlide = 0;
   // maximum number of slides
   let maxSlide = slides.length - 1;
- 
+
+  const slider = document.querySelector(".slider");
   //Auto play
-  setInterval(nextS, 4000);
+  const inter = setInterval(nextS, 5000);
+  slider.addEventListener("mouseenter", () => {
+    clearInterval(inter);
+    console.log("stopped");
+  });
+  slider.addEventListener("mouseleave", () => {
+    setInterval(nextS, 5000);
+    console.log("restart");
+  });
 
-
-  nextSlide.addEventListener("click", nextS)
+  nextSlide.addEventListener("click", nextS);
   function nextS() {
     // check if current slide is the last and reset current slide
     if (curSlide === maxSlide) {
@@ -31,12 +38,11 @@ document.addEventListener("DOMContentLoaded", function () {
       sliderItem.style.transform = `translateX(${200 * (indx - curSlide)}%)`;
     });
     slides[curSlide].classList.add("vis");
-    if(curSlide-1>=0)slides[curSlide-1].classList.remove("vis");
-  };
+    if (curSlide - 1 >= 0) slides[curSlide - 1].classList.remove("vis");
+  }
   // select prev slide button
   const prevSlide = document.querySelector(".btnR");
 
-  
   prevSlide.addEventListener("click", function () {
     // check if current slide is the first and reset current slide to last
     if (curSlide === 0) {
@@ -50,6 +56,6 @@ document.addEventListener("DOMContentLoaded", function () {
       sliderItem.style.transform = `translateX(${200 * (indx - curSlide)}%)`;
     });
     slides[curSlide].classList.add("vis");
-    if(curSlide+1<=maxSlide) slides[curSlide+1].classList.remove("vis");
+    if (curSlide + 1 <= maxSlide) slides[curSlide + 1].classList.remove("vis");
   });
 });
